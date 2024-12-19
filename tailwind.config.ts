1e8a809a1146a52d0623e3be689bcc8c4ed05a2e
adhikari-samir <adhikarysameer10@gmail.com>
2024-12-19 08:43:07 +0545

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,44 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Includes all files in the app directory
+    "./src/Component/**/*.{js,ts,jsx,tsx,mdx}", // Includes all files in the Component directory
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // Includes all files in the pages directory (if used)
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}", // Includes all files in the utils directory
   ],
+
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+     
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1025px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontWeight: {
+        regular: "400",
+        bold: "700",
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+      Playfair: ["Playfair Display", "sans-serif"],
+      },
+      textcolor:{
+      lightblack: "#767E86",
+      orangeball:" #FF7757",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
